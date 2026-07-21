0137d708980e0483ca291dd4abe29f0ed7601559
Anmol Sharma <anmolsharma0541@gmail.com>
2026-07-21 18:16:48 +0530

    First unsorted element in a sorted array

--- 1-Array/6-first_unsorted_element.java
+++ 1-Array/6-first_unsorted_element.java
@@ -1,11 +1,12 @@
-public static int firstUnsorted(int[] arr) {
-    for(int i=0;i<arr.length-1;i++) {
-        if(arr[i]>arr[i+1]){
-            return arr[i+1];
+class Solution {
+    public static int firstUnsorted(int[] arr) {
+        for(int i=0;i<arr.length-1;i++) {
+            if(arr[i]>arr[i+1]){
+                return arr[i+1];
+            }
         }
+        return -1;
     }
-    return -1;
-}
 // Problem: First unsorted element in a sorted array 
 // Example: [2,5,12,9,15]→ 9
 // Approach: Traverse the array and return the first element where the current element is greater than the next element.
